f608b84c022993735fc3fc442f943738ea9248ca
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-03-28 22:47:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
- import React from "react";
+ import React, { useRef, useState } from "react";
 import "../styles/App.css";
-import useInput from "./useInput";
-import SignInForm from "./SignInForm";
-const App = () => {
-  //complete the useInput hook
-  const email = useInput("");
-  const password = useInput("");
+import useLocalStorage from "./useLocalStorage";
 
+const App = () => {
+  const [state, setState] = useLocalStorage("key", null);
+  const inputRef = useRef(null);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const text = inputRef.current.value;
+    setState(text);
+  };
 
   return (
     <div id="main">
-      <SignInForm email={email} password={password} />
-      <h1>Result</h1>
-      <h2 id="email">Email is: {email.value}</h2>
-      <h2 id="password" >Password is: {password.value}</h2>
+      <label>Enter a Value to save in localStorage</label>
+      <input type="text" id="value" ref={inputRef} />
+      <button id="set" onClick={submitHandler}>
+        Set Value
+      </button>
     </div>
   );
 };
