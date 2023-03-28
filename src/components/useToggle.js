585b9d63b4f06a1c4d98d6bef4fd0eea71dfdd08
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-03-28 22:49:25 +0530

    Update useToggle.js

--- src/components/useToggle.js
+++ src/components/useToggle.js
@@ -1,7 +1,14 @@
-import React,{useCallback,useState} from 'react'
+ import { useEffect } from "react";
+import { useState } from "react";
 
-const useToggle = () => {
-   
-  
-  }
-  export default useToggle;
+const useLocalStorage = (stateVar, value) => {
+  const[curState,setState] = useState(value);
+  updateStorage(stateVar,curState);
+  return [curState,setState];
+};
+
+const updateStorage =(key,val)=>{
+    localStorage.setItem(key,val);
+}
+
+export default useLocalStorage;
