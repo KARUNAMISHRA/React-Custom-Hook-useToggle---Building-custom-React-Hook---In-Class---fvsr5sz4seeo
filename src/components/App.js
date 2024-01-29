e9d7e9bfb88aa462b6983b232af36146aad62341
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2024-01-29 12:55:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,16 @@
- import React, { useRef, useState } from "react";
-import "../styles/App.css";
-import useLocalStorage from "./useLocalStorage";
-
+import React,{useCallback,useState} from 'react'
+import '../styles/App.css';
+import useToggle from './useToggle.js'
 const App = () => {
-  const [state, setState] = useLocalStorage("key", null);
-  const inputRef = useRef(null);
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const text = inputRef.current.value;
-    setState(text);
-  };
+  
 
-  return (
-    <div id="main">
-      <label>Enter a Value to save in localStorage</label>
-      <input type="text" id="value" ref={inputRef} />
-      <button id="set" onClick={submitHandler}>
-        Set Value
-      </button>
-    </div>
-  );
-};
+    const [isTextChanged, setIsTextChanged] = useToggle(false);
+
+  return(
+        <button id='button' onClick={setIsTextChanged}>{isTextChanged ? 'Toggled' : 'Click to Toggle'}</button>
+    );
+}
+
 
 export default App;
